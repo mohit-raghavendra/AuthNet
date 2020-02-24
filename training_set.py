968680e9bc8906a5cc8a88874348f6e8f375b3e6
Mohit-Mithra <mohithmitra@gmail.com>
2020-02-24 16:11:34 +0530

    Multilabel labelling algorithm

---
+++ training_set.py
@@ -0,0 +1,28 @@
+
+def create_multiclass_labels():
+
+	#for X_train and y_train
+	for i in range(no of samples):
+		if i>= 0 and i<=9:
+			#add it to class 1
+		else 
+			if i>= 10 and i <=99:
+			#add it to class 2
+			else 
+				if i%100 < 10:
+					#add it to class 3
+					else 
+						#add it to class 4  
+
+
+
+def create_binary_labels():
+
+	#for X_train and y_train
+	for i in range(no of samples):
+		if i>= 0 and i<=9:
+			#add it to class 1
+		else 
+			#add it to class 0
+ 
+
