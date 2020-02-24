deb62bc7d4c9c1a394b34e65276097786956ac07
Mohit-Mithra <mohithmitra@gmail.com>
2020-02-24 18:54:10 +0530

    binary labeller

--- training_set.py
+++ training_set.py
@@ -1,18 +1,27 @@
 
 def create_multiclass_labels():
-
+	y_train = []
+	a = 0
+	b = 0
+	c = 0
+	d = 0
 	#for X_train and y_train
 	for i in range(no of samples):
 		if i>= 0 and i<=9:
 			#add it to class 1
+			y_train[i] = 1
+			
 		else 
 			if i>= 10 and i <=99:
-			#add it to class 2
+			#add it to class 
+			y_train[i] = 2
 			else 
 				if i%100 < 10:
 					#add it to class 3
-					else 
+					y_train = 3
+					else: 
 						#add it to class 4  
+						y_train = 4
 
 
 
