cb8f82c3d80c42845e896eb0be24d18d263017e9
Mohit-Mithra <mohithmitra@gmail.com>
2020-04-08 14:37:21 +0530

    visualise loss and acc

--- Library/callbacks.py
+++ Library/callbacks.py
@@ -19,3 +19,21 @@ print(history.history['loss'])
 
 print(history.history['val_acc'])
 print(history.history['val_loss'])
+
+
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
